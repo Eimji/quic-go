costinm/quic

implement the new variable integer length frame types

internal/wire/max_data_frame_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,19 +12,18 @@ import (
 var _ = Describe("MAX_DATA frame", func() {
 	Context("when parsing", func() {
 		It("accepts sample frame", func() {
-			b := bytes.NewReader([]byte{0x4,
-				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
-			})
+			data := []byte{0x4}
+			data = append(data, encodeVarInt(0xdecafbad123456)...) // byte offset
+			b := bytes.NewReader(data)
 			frame, err := ParseMaxDataFrame(b, versionBigEndian)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad123456)))
 			Expect(b.Len()).To(BeZero())
 		})
 
 		It("errors on EOFs", func() {
-			data := []byte{0x4,
-				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
-			}
+			data := []byte{0x4}
+			data = append(data, encodeVarInt(0xdecafbad1234567)...) // byte offset
 			_, err := ParseMaxDataFrame(bytes.NewReader(data), versionMaxDataFrame)
 			Expect(err).NotTo(HaveOccurred())
 			for i := range data {
@@ -38,19 +38,19 @@ var _ = Describe("MAX_DATA frame", func() {
 			f := &MaxDataFrame{
 				ByteOffset: 0xdeadbeef,
 			}
-			Expect(f.MinLength(versionMaxDataFrame)).To(Equal(protocol.ByteCount(1 + 8)))
+			Expect(f.MinLength(versionMaxDataFrame)).To(Equal(1 + utils.VarIntLen(0xdeadbeef)))
 		})
 
 		It("writes a MAX_DATA frame", func() {
 			b := &bytes.Buffer{}
 			f := &MaxDataFrame{
-				ByteOffset: 0xdeadbeefcafe1337,
+				ByteOffset: 0xdeadbeefcafe,
 			}
 			err := f.Write(b, versionMaxDataFrame)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(b.Bytes()).To(Equal([]byte{0x4,
-				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
-			}))
+			expected := []byte{0x4}
+			expected = append(expected, encodeVarInt(0xdeadbeefcafe)...)
+			Expect(b.Bytes()).To(Equal(expected))
 		})
 	})
 })

internal/wire/max_stream_data_frame.go
@@ -22,13 +22,13 @@ func ParseMaxStreamDataFrame(r *bytes.Reader, version protocol.VersionNumber) (*
 		return nil, err
 	}
 
-	sid, err := utils.GetByteOrder(version).ReadUint32(r)
+	sid, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
 	frame.StreamID = protocol.StreamID(sid)
 
-	byteOffset, err := utils.GetByteOrder(version).ReadUint64(r)
+	byteOffset, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
@@ -45,12 +45,16 @@ func (f *MaxStreamDataFrame) Write(b *bytes.Buffer, version protocol.VersionNumb
 		}).Write(b, version)
 	}
 	b.WriteByte(0x5)
-	utils.GetByteOrder(version).WriteUint32(b, uint32(f.StreamID))
-	utils.GetByteOrder(version).WriteUint64(b, uint64(f.ByteOffset))
+	utils.WriteVarInt(b, uint64(f.StreamID))
+	utils.WriteVarInt(b, uint64(f.ByteOffset))
 	return nil
 }
 
 // MinLength of a written frame
 func (f *MaxStreamDataFrame) MinLength(version protocol.VersionNumber) (protocol.ByteCount, error) {
-	return 1 + 4 + 8, nil
+	// writing this frame would result in a gQUIC WINDOW_UPDATE being written, which has a different length
+	if !version.UsesMaxDataFrame() {
+		return 1 + 4 + 8, nil
+	}
+	return 1 + utils.VarIntLen(uint64(f.StreamID)) + utils.VarIntLen(uint64(f.ByteOffset)), nil
 }

internal/wire/stream_blocked_frame.go
@@ -14,18 +14,14 @@ type StreamBlockedFrame struct {
 
 // ParseStreamBlockedFrame parses a STREAM_BLOCKED frame
 func ParseStreamBlockedFrame(r *bytes.Reader, version protocol.VersionNumber) (*StreamBlockedFrame, error) {
-	frame := &StreamBlockedFrame{}
-
-	// read the TypeByte
-	if _, err := r.ReadByte(); err != nil {
+	if _, err := r.ReadByte(); err != nil { // read the TypeByte
 		return nil, err
 	}
-	sid, err := utils.GetByteOrder(version).ReadUint32(r)
+	sid, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
-	frame.StreamID = protocol.StreamID(sid)
-	return frame, nil
+	return &StreamBlockedFrame{StreamID: protocol.StreamID(sid)}, nil
 }
 
 // Write writes a STREAM_BLOCKED frame
@@ -34,11 +30,14 @@ func (f *StreamBlockedFrame) Write(b *bytes.Buffer, version protocol.VersionNumb
 		return (&blockedFrameLegacy{StreamID: f.StreamID}).Write(b, version)
 	}
 	b.WriteByte(0x09)
-	utils.GetByteOrder(version).WriteUint32(b, uint32(f.StreamID))
+	utils.WriteVarInt(b, uint64(f.StreamID))
 	return nil
 }
 
 // MinLength of a written frame
 func (f *StreamBlockedFrame) MinLength(version protocol.VersionNumber) (protocol.ByteCount, error) {
-	return 1 + 4, nil
+	if !version.UsesMaxDataFrame() {
+		return 1 + 4, nil
+	}
+	return 1 + utils.VarIntLen(uint64(f.StreamID)), nil
 }

Eimji/quic-go

internal/wire/max_data_frame_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,19 +12,18 @@ import (
 var _ = Describe("MAX_DATA frame", func() {
 	Context("when parsing", func() {
 		It("accepts sample frame", func() {
-			b := bytes.NewReader([]byte{0x4,
-				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
-			})
+			data := []byte{0x4}
+			data = append(data, encodeVarInt(0xdecafbad123456)...) // byte offset
+			b := bytes.NewReader(data)
 			frame, err := ParseMaxDataFrame(b, versionBigEndian)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad123456)))
 			Expect(b.Len()).To(BeZero())
 		})
 
 		It("errors on EOFs", func() {
-			data := []byte{0x4,
-				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
-			}
+			data := []byte{0x4}
+			data = append(data, encodeVarInt(0xdecafbad1234567)...) // byte offset
 			_, err := ParseMaxDataFrame(bytes.NewReader(data), versionIETFFrames)
 			Expect(err).NotTo(HaveOccurred())
 			for i := range data {
@@ -38,19 +38,19 @@ var _ = Describe("MAX_DATA frame", func() {
 			f := &MaxDataFrame{
 				ByteOffset: 0xdeadbeef,
 			}
-			Expect(f.MinLength(versionIETFFrames)).To(Equal(protocol.ByteCount(1 + 8)))
+			Expect(f.MinLength(versionIETFFrames)).To(Equal(1 + utils.VarIntLen(0xdeadbeef)))
 		})
 
 		It("writes a MAX_DATA frame", func() {
 			b := &bytes.Buffer{}
 			f := &MaxDataFrame{
-				ByteOffset: 0xdeadbeefcafe1337,
+				ByteOffset: 0xdeadbeefcafe,
 			}
 			err := f.Write(b, versionIETFFrames)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(b.Bytes()).To(Equal([]byte{0x4,
-				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
-			}))
+			expected := []byte{0x4}
+			expected = append(expected, encodeVarInt(0xdeadbeefcafe)...)
+			Expect(b.Bytes()).To(Equal(expected))
 		})
 	})
 })

internal/wire/max_stream_data_frame.go
@@ -22,13 +22,13 @@ func ParseMaxStreamDataFrame(r *bytes.Reader, version protocol.VersionNumber) (*
 		return nil, err
 	}
 
-	sid, err := utils.GetByteOrder(version).ReadUint32(r)
+	sid, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
 	frame.StreamID = protocol.StreamID(sid)
 
-	byteOffset, err := utils.GetByteOrder(version).ReadUint64(r)
+	byteOffset, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
@@ -45,12 +45,16 @@ func (f *MaxStreamDataFrame) Write(b *bytes.Buffer, version protocol.VersionNumb
 		}).Write(b, version)
 	}
 	b.WriteByte(0x5)
-	utils.GetByteOrder(version).WriteUint32(b, uint32(f.StreamID))
-	utils.GetByteOrder(version).WriteUint64(b, uint64(f.ByteOffset))
+	utils.WriteVarInt(b, uint64(f.StreamID))
+	utils.WriteVarInt(b, uint64(f.ByteOffset))
 	return nil
 }
 
 // MinLength of a written frame
 func (f *MaxStreamDataFrame) MinLength(version protocol.VersionNumber) (protocol.ByteCount, error) {
-	return 1 + 4 + 8, nil
+	// writing this frame would result in a gQUIC WINDOW_UPDATE being written, which has a different length
+	if !version.UsesIETFFrameFormat() {
+		return 1 + 4 + 8, nil
+	}
+	return 1 + utils.VarIntLen(uint64(f.StreamID)) + utils.VarIntLen(uint64(f.ByteOffset)), nil
 }

internal/wire/stream_blocked_frame.go
@@ -14,18 +14,14 @@ type StreamBlockedFrame struct {
 
 // ParseStreamBlockedFrame parses a STREAM_BLOCKED frame
 func ParseStreamBlockedFrame(r *bytes.Reader, version protocol.VersionNumber) (*StreamBlockedFrame, error) {
-	frame := &StreamBlockedFrame{}
-
-	// read the TypeByte
-	if _, err := r.ReadByte(); err != nil {
+	if _, err := r.ReadByte(); err != nil { // read the TypeByte
 		return nil, err
 	}
-	sid, err := utils.GetByteOrder(version).ReadUint32(r)
+	sid, err := utils.ReadVarInt(r)
 	if err != nil {
 		return nil, err
 	}
-	frame.StreamID = protocol.StreamID(sid)
-	return frame, nil
+	return &StreamBlockedFrame{StreamID: protocol.StreamID(sid)}, nil
 }
 
 // Write writes a STREAM_BLOCKED frame
@@ -34,11 +30,14 @@ func (f *StreamBlockedFrame) Write(b *bytes.Buffer, version protocol.VersionNumb
 		return (&blockedFrameLegacy{StreamID: f.StreamID}).Write(b, version)
 	}
 	b.WriteByte(0x09)
-	utils.GetByteOrder(version).WriteUint32(b, uint32(f.StreamID))
+	utils.WriteVarInt(b, uint64(f.StreamID))
 	return nil
 }
 
 // MinLength of a written frame
 func (f *StreamBlockedFrame) MinLength(version protocol.VersionNumber) (protocol.ByteCount, error) {
-	return 1 + 4, nil
+	if !version.UsesIETFFrameFormat() {
+		return 1 + 4, nil
+	}
+	return 1 + utils.VarIntLen(uint64(f.StreamID)), nil
 }